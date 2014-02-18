bf9f770c612ba5d3bacdf6b11ddf4020927f7400
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-02-17 22:01:50 -0800

    -n flake8, comments, and import_log* helpers

--- pynux/utils.py
+++ pynux/utils.py
@@ -19,12 +19,21 @@ from time import sleep
 
 class Nuxeo:
     def __init__(self, conf={}):
+        """configuration for http connections"""
         # http://stackoverflow.com/a/17501381/1763984
         defaults = {
-            "user":         os.environ.get('NUXEO_API_USER',         "Administrator"),
-            "password":     os.environ.get('NUXEO_API_PASS',         "Administrator"),
-            "api":          os.environ.get('NUXEO_REST_API',         "http://localhost:8080/nuxeo/site/api/v1"),
-            "fileImporter": os.environ.get('NUXEO_FILEIMPORTER_API', "http://localhost:8080/nuxeo/site/fileImporter"),
+            "user":
+            os.environ.get('NUXEO_API_USER',
+                           "Administrator"),
+            "password":
+            os.environ.get('NUXEO_API_PASS',
+                           "Administrator"),
+            "api":
+            os.environ.get('NUXEO_REST_API',
+                           "http://localhost:8080/nuxeo/site/api/v1"),
+            "fileImporter":
+            os.environ.get('NUXEO_FILEIMPORTER_API',
+                           "http://localhost:8080/nuxeo/site/fileImporter")
         }
         self.conf = {}
         self.conf.update(defaults)
@@ -32,6 +41,7 @@ class Nuxeo:
         self.auth = (self.conf["user"], self.conf["password"])
 
     def _recursive_get(self, url, params, documents, current_page_index):
+        """keep paging results from nuxeo until we are done"""
         params.update({'currentPageIndex': current_page_index})
         res = requests.get(url, params=params, auth=self.auth)
         res.raise_for_status()
@@ -48,7 +58,7 @@ class Nuxeo:
     # REST API
 
     def nxql(self, query):
-        """page through the results for an nxql query"""
+        """generice nxql query"""
         url = os.path.join(self.conf["api"], "path/@search")
         params = {
             'pageSize': '100',
@@ -59,21 +69,23 @@ class Nuxeo:
         return documents
 
     def all(self):
-        """page through the results for all"""
+        """.nxql("SELECT * FROM Document")"""
         return self.nxql('SELECT * FROM Document')
 
     def children(self, path):
-        path = path.strip("/")
         """get child documents of a path"""
-        url = os.path.join(self.conf["api"],  "path", path,  "@children")
+        url = os.path.join(self.conf["api"],
+                           "path",
+                           path.strip("/"),
+                           "@children")
         params = {}
         documents = []
         self._recursive_get(url, params, documents, 1)
         return documents
 
     def get_uid(self, path):
-        path = path.strip("/")
         """look up uid from the path"""
+        path = path.strip("/")
         url = os.path.join(self.conf['api'],  "path", path)
         res = requests.get(url, auth=self.auth)
         res.raise_for_status()
@@ -85,7 +97,9 @@ class Nuxeo:
             raise TypeError("either uid or path")
         url = ""
         if 'path' in documentid:
-            url = os.path.join(self.conf['api'], "path", documentid['path'].strip("/"))
+            url = os.path.join(self.conf['api'],
+                               "path",
+                               documentid['path'].strip("/"))
         elif 'uid' in documentid:
             url = os.path.join(self.conf['api'], "id", documentid['uid'])
         else:
@@ -105,6 +119,7 @@ class Nuxeo:
         elif 'uid' in documentid:
             uid = documentid['uid']
         url = os.path.join(self.conf['api'], "id", uid)
+        # TODO: let headers be specified, use these as defaults
         headers = {'X-NXDocumentProperties': 'ucldc_schema,dublincore',
                    'Content-Type': 'application/json+nxentity'}
         # copy what we want from the input json into the payload
@@ -120,12 +135,20 @@ class Nuxeo:
 
     # platform importer api
 
-    def call_file_importer_api(self, verb, params):
+    def call_file_importer_api(self, verb, params={}):
         """generic wrapper to make GET calls to this API"""
         url = "{0}/{1}".format(self.conf['fileImporter'], verb)
         res = requests.get(url, params=params, auth=self.auth)
         res.raise_for_status()
         return res.text
+
+    def import_log(self):
+        """show small part of file importer log"""
+        print self.call_file_importer_api("log")
+
+    def import_log_activate(self):
+        """activate file importer log"""
+        print self.call_file_importer_api("logActivate")
 
     def import_one_folder(self,
                           leaf_type, input_path, target_path, folderish_type):
@@ -141,10 +164,11 @@ class Nuxeo:
         print self.call_file_importer_api("run", params)
         # an import should now be running, only one import can run at a time
         # poll the api to and wait for the run to finish...
-        self.wait_file_importer()
+        self.import_status_wait()
         return
 
-    def wait_file_importer(self):
+    def import_status_wait(self):
+        """check import status and wait for Not Running"""
         url = "{0}/{1}".format(self.conf['fileImporter'], "status")
         res = requests.get(url, auth=self.auth)
         res.raise_for_status()
