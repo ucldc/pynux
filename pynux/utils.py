c2b0826da4daebf032d494bcededdf9ec72b6d89
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-02-21 14:56:54 -0800

    flake8 and copyright

--- pynux/utils.py
+++ pynux/utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: ascii -*-
+# -*- coding: utf8 -*-
 
 """
 pynux.utils
@@ -14,6 +14,7 @@ import json
 import sys
 import os
 from time import sleep
+
 
 class Nuxeo:
     def __init__(self, conf={}):
@@ -60,7 +61,7 @@ class Nuxeo:
         if os.path.isdir(newdir):
             pass
         elif os.path.isfile(newdir):
-            raise OSError("a file with the same name as the desired " \
+            raise OSError("a file with the same name as the desired "
                           "dir, '%s', already exists." % newdir)
         else:
             head, tail = os.path.split(newdir)
@@ -148,11 +149,9 @@ class Nuxeo:
         res.raise_for_status()
         return json.loads(res.text)
 
-
     def print_document_summary(self, documents):
         for document in documents:
             print "{0}\t{1}".format(document['uid'], document['path'])
-
 
     def copy_metadata_to_local(self, documents, local):
         for document in documents:
@@ -161,18 +160,17 @@ class Nuxeo:
             file = os.path.join(local, ''.join([path.strip("/"), ".json"]))
             dir = os.path.dirname(file)
             self._mkdir(dir)
-            with open(file, 'w') as json_file: 
-                py_json = self.get_metadata(path=path);
+            with open(file, 'w') as json_file:
+                py_json = self.get_metadata(path=path)
                 out_json = {}
-                out_json['uid']=py_json['uid']
-                out_json['path']=py_json['path']
-                out_json["entity-type"]=py_json["entity-type"]
-                out_json['properties']=py_json['properties']
+                out_json['uid'] = py_json['uid']
+                out_json['path'] = py_json['path']
+                out_json["entity-type"] = py_json["entity-type"]
+                out_json['properties'] = py_json['properties']
                 json_file.write(json.dumps(out_json,
                                            sort_keys=True,
-                                           indent=4, 
+                                           indent=4,
                                            separators=(',', ': ')))
-
 
     # platform importer api
 
@@ -228,3 +226,32 @@ def test():
 
 if __name__ == '__main__':
     test()
+
+"""
+Copyright © 2014, Regents of the University of California
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+- Neither the name of the University of California nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
