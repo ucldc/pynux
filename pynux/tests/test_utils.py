9e39b634ca3f1453255dc5470c3cc6e443fbcb92
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-03-04 20:06:38 -0800

    test is set up a little better

--- pynux/tests/test_utils.py
+++ pynux/tests/test_utils.py
@@ -1,6 +1,12 @@
 from httmock import all_requests, HTTMock
 from pynux import utils
 import json
+import sys
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
 
 @all_requests
 def response_content(url, request):
@@ -13,7 +19,9 @@ def response_content(url, request):
     return {'status_code': 200,
             'content': json.dumps(content) }
 
-with HTTMock(response_content):
-    nx = utils.Nuxeo()
-    assert(nx.all())
-    assert(nx.all().next())
+class TestNuxeoFunctions(unittest.TestCase):
+    def test_this(self):
+        with HTTMock(response_content):
+            nx = utils.Nuxeo()
+            assert(nx.all())
+            assert(nx.all().next())
