0088522d68e67348196c8b456ace7626fbea3012
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-03-04 21:19:37 -0800

    slightly more test

--- pynux/tests/test_utils.py
+++ pynux/tests/test_utils.py
@@ -9,22 +9,33 @@ if sys.version_info < (2, 7):
 else:
     import unittest
 
+
 class TestNuxeoREST(unittest.TestCase):
     def setUp(self):
         self.nx = utils.Nuxeo()
 
     @httpretty.activate
     def test_this(self):
-        content = {
-            'entries': [{
-                'properties': [],
-            }],
+        document = {
+            'properties': [],
+            'uid': 'xxxx'
+        }
+        results = {
+            'entries': [ document ],
             'isNextPageAvailable': False
         }
         httpretty.register_uri(
             httpretty.GET,
-            re.compile("(\w+)"),
-            body=json.dumps(content),
+            re.compile("(\w+)/@\w+"),
+            body=json.dumps(results),
+        )
+        httpretty.register_uri(
+            httpretty.GET,
+            re.compile("^((?!\@).)*$"),  # http://stackoverflow.com/a/406408/1763984
+            body=json.dumps(document),
         )
         assert(self.nx.all())
         assert(self.nx.all().next())
+        assert(self.nx.children("asset-library"))
+        assert(self.nx.get_metadata(uid= self.nx.get_uid("asset-library")))
+        assert(self.nx.get_metadata(path="asset-library"))
